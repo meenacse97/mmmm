61edf086b3584031d2635de91822d9885099f5b0
meenacseengineering5 <meenacse1997@gmail.com>
2016-12-20 15:49:39 +0530

    Add files via upload

---
+++ meena.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("\n enter th no:");
+    scnf("%d",&num);
+    if(a%2==0)
+    {
+
+        printf("\n %d is a even number");
+    }
+    else
+        {
+        printf("\n %d is a odd number");
+    }
+    getch();
+    }
+
